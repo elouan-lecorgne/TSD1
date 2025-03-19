c426fd9116a97648be0f2a465930df1a344c36c5
Elouan Lecorgne <elouanlecorgne@MacBook-Air-de-Elouan.local>
2025-03-19 17:46:56 +0100

    Sufficient Task finished LINQ

--- 03-LINQ/GoldSavings.App/Model/GoldPrice.cs
+++ 03-LINQ/GoldSavings.App/Model/GoldPrice.cs
@@ -1,12 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using Newtonsoft.Json;
 
-namespace GoldSavings.App.Model;
-
-public class GoldPrice
+namespace GoldSavings.App.Model
 {
-    [JsonProperty("Data")]
-    public DateTime Date { get; set; }
+    public class GoldPrice
+    {
+        [JsonProperty("Data")]
+        public DateTime Date { get; set; }
 
-    [JsonProperty("Cena")]
-    public double Price { get; set; }
+        [JsonProperty("Cena")]
+        public double Price { get; set; }
+    }
+
+    public class GoldPriceSerializer
+    {
+        public static void SaveToXml(List<GoldPrice> prices, string filePath)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<GoldPrice>));
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    serializer.Serialize(writer, prices);
+                }
+
+                Console.WriteLine($"Gold prices saved to {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving to XML: {ex.Message}");
+            }
+        }
+
+        public static List<GoldPrice> LoadFromXml(string filePath) => (List<GoldPrice>)new XmlSerializer(typeof(List<GoldPrice>)).Deserialize(new StreamReader(filePath));
+
+    }
 }
